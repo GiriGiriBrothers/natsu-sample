576902471b756b68aa6ca24115c5da8a1cf14bee
swanmatch <swanmatch@yahoo.co.jp>
2021-09-03 22:22:39 +0900

    uhn

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
 module.exports = {
   images: {
     loader: "imgix",
-    path: urlPrefix,
+    path: 'http://gitrigiribrothers.github.io/natsu-sample',
   },
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
