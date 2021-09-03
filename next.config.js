80433b8a1f1ae8fc1ae9f3d780b9536423986919
swanmatch <swanmatch@yahoo.co.jp>
2021-09-03 22:09:26 +0900

    uhn

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
+
 module.exports = {
   images: {
     loader: "imgix",
     path: 'http://gitrigiribrothers.github.io',
   },
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
   reactStrictMode: true,
 }
 
